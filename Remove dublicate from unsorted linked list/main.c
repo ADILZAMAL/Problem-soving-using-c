5dce5ce1302c7666d6ed82e591e6a0341e268493
ADILZAMAL <adilzamal@gmail.com>
2019-03-05 02:11:16 +0530

    Remove dublicate in usorte linked list

--- Remove dublicate from unsorted linked list/main.c
+++ Remove dublicate from unsorted linked list/main.c
@@ -1,75 +1,78 @@
-    #include <stdio.h>
-    #include <stdlib.h>
-    struct Node{
-    int data;
-    struct Node *next;
-    };
-    struct Node *headNode=NULL;
+     //Remove dublicate from unsorted linked list//       
 
 
-    void insert(int num){
-    struct Node *temp=(struct Node*)malloc(sizeof(struct Node));
-    temp->data=num;
-    temp->next=NULL;
-    if(headNode==NULL){
-        headNode=temp;
-        return ;
-    }
-    struct Node *temp1=headNode;
-    while(temp1->next!=NULL)
-        temp1=temp1->next;
-    temp1->next=temp;
-    }
+        #include <stdio.h>
+        #include <stdlib.h>
+        struct Node{
+        int data;
+        struct Node *next;
+        };
+        struct Node *headNode=NULL;
 
-    void print(){
-        struct Node *temp;
-        temp=headNode;
-        while(temp!=NULL){
-            printf("%d\t",temp->data);
-            temp=temp->next;
+
+        void insert(int num){
+        struct Node *temp=(struct Node*)malloc(sizeof(struct Node));
+        temp->data=num;
+        temp->next=NULL;
+        if(headNode==NULL){
+            headNode=temp;
+            return ;
         }
-    }
-    void RemoveDublicate(){
-        struct Node *temp=headNode,*prev,*del;
-        struct Node *temp1=NULL;
-        while(temp!=NULL){
-                temp1=temp->next;
-                prev=temp;
-          while(temp1!=NULL)
-          {
-              if(temp->data==temp1->data)
-              {
-                  prev->next=temp1->next;
-                  del=temp1;
-                  temp1=temp1->next;
-                  free(del);
-                  }
+        struct Node *temp1=headNode;
+        while(temp1->next!=NULL)
+            temp1=temp1->next;
+        temp1->next=temp;
+        }
 
-              else
-                   {
+        void print(){
+            struct Node *temp;
+            temp=headNode;
+            while(temp!=NULL){
+                printf("%d\t",temp->data);
+                temp=temp->next;
+            }
+        }
+        void RemoveDublicate(){
+            struct Node *temp=headNode,*prev,*del;
+            struct Node *temp1=NULL;
+            while(temp!=NULL){
+                    temp1=temp->next;
+                    prev=temp;
+            while(temp1!=NULL)
+            {
+                if(temp->data==temp1->data)
+                {
+                    prev->next=temp1->next;
+                    del=temp1;
                     temp1=temp1->next;
-                    prev=prev->next;
-                   }
+                    free(del);
+                    }
+
+                else
+                    {
+                        temp1=temp1->next;
+                        prev=prev->next;
+                    }
 
 
-          }
-          temp=temp->next;
+            }
+            temp=temp->next;
+            }
         }
-    }
 
 
-    int main()
-    {
-        insert(10);
-        insert(11);
-        insert(11);
-        insert(12);
-        insert(11);
-        insert(5);
-        insert(3);
-        insert(4);
-        insert(4);
-        RemoveDublicate();
-        print();
+        int main()
+        {
+            insert(10);
+            insert(11);
+            insert(11);
+            insert(12);
+            insert(11);
+            insert(5);
+            insert(3);
+            insert(4);
+            insert(4);
+            RemoveDublicate();
+            print();
 
-    }
+        }
