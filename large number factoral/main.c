48d0a7f0fdea0f7ccc8004e405aa35c19ea195e3
ADILZAMAL <adilzamal@gmail.com>
2019-03-09 23:21:19 +0530

    Factorial of large number

---
+++ large number factoral/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i;
+    long int result;
+    scanf("%d",&n);
+    int array[160];
+    for(i=0;i<160;i++)
+        array[i]=0;
+        array[159]=1;
+
+
+}
