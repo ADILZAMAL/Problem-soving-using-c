5213671f477cd26c5f22b773c16dcdd920d35ded
ADILZAMAL <adilzamal@gmail.com>
2019-07-07 12:47:51 +0530

    pattern printing

--- pattern printing/main.c
+++ pattern printing/main.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
+int n;
+void rec(int a)
+{
+    printf("%d ",a);
+    if(a<=0)
+        return ;
+    else
+        rec(a-5);
+        printf("%d ",a);
+
+
+}
 
 int main()
 {
-int num,i,j;
-printf("Enter number of lines");
-scanf("%d",&num);
-for(i=0;i<num;i++)
-{
-printf("1");
-for(j=1;j<i;j++){
-        if(i==num-1){
-            printf("%d",j+1);
-            continue;
-        }
-    printf("0");
-}
-if(i>0)
-    printf("%d",i+1);
-printf("\n");
-}
-
-
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&n);
+        rec(n);
+    }
+	//code
+	return 0;
 }
